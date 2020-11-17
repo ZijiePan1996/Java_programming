175f8493ad427af6a11ff462eff9532b65bb82f6
Zijie Pan <dreamsky627@icloud.com>
2020-11-17 17:46:10 +0800

    11.17每日算法练习

---
+++ src/ClassDemo/Algorithms/SearchMatrix.java
@@ -0,0 +1,40 @@
+package ClassDemo.Algorithms;
+
+public class SearchMatrix {
+    public static boolean searchMatrixForce(int[][] matrix, int target){
+        //暴力算法复杂度自然是O（mn），空间复杂度O（1）
+        for(int i =0; i< matrix.length;i++){
+            for(int j =0; j<matrix[i].length; j++){
+                if(matrix[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean searchMatrixPointer(int[][] matrix, int target){
+        int i = matrix.length-1;
+        int j = 0;
+        if(matrix == null) return false;
+        while(j<matrix[0].length && i>0){
+            if(matrix[i][j] > target)  i--;
+            else if(matrix[i][j] < target)  j++;
+            else return true;
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        int[][] newmatrix = new int[][]{
+                {1,   4,  7, 11, 15},
+            {2,   5,  8, 12, 19},
+            {3,   6,  9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+        System.out.println(searchMatrixForce(newmatrix, 20));
+        System.out.println(searchMatrixPointer(newmatrix, 20));
+    }
+}
