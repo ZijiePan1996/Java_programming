86abf1d7d7e2cd0c4ac44f7e92b0ba1a0b222333
Zijie Pan <dreamsky6270@icloud.com>
2020-12-13 17:38:59 +0800

    每日算法练习： 12.13

--- src/ClassDemo/Algorithms/Search/Robots.java
+++ src/ClassDemo/Algorithms/Search/Robots.java
@@ -1,7 +1,9 @@
 package ClassDemo.Algorithms.Search;
 
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class Robots {
     /*剑指offer13，机器人的运动范围
@@ -12,6 +14,8 @@ public class Robots {
      */
     public static int movingCount(int m, int n, int k) {
         Deque<int[]> queue = new LinkedList<>();
+        Set<int[]> hashset = new HashSet<>();
+        boolean[][] vis = new boolean[n][m];
         queue.offerLast(new int[]{0, 0});
         int result = 0;
         while (!queue.isEmpty()) {
@@ -19,11 +23,19 @@ public class Robots {
             for (int i = 0; i < length; i++) {
                 result++;
                 int[] curr = queue.pollFirst();
-                if (curr[0] + 1 < m && countSum(curr[0] + 1) + countSum(curr[1]) <= k) {
-                    queue.offerLast(new int[]{curr[0] + 1, curr[1]});
+                if (curr[1] + 1 < m && countSum(curr[0] ) + countSum(curr[1] + 1) <= k) {
+                    int[] rnode = new int[]{curr[0], curr[1]+1};
+                    if(!vis[rnode[0]][rnode[1]]){
+                        vis[rnode[0]][rnode[1]] = true;
+                        queue.offerLast(rnode);
+                    }
                 }
-                if (curr[1] + 1 < n && countSum(curr[0] ) + countSum(curr[1] + 1) <= k) {
-                    queue.offerLast(new int[]{curr[0] , curr[1] + 1});
+                if (curr[0] + 1 < n && countSum(curr[0] + 1) + countSum(curr[1]) <= k) {
+                    int[] dnode = new int[]{curr[0] + 1, curr[1]};
+                    if(!vis[dnode[0]][dnode[1]]){
+                        vis[dnode[0]][dnode[1]] = true;
+                        queue.offerLast(dnode);
+                    }
                 }
             }
         }
@@ -42,6 +54,6 @@ public class Robots {
     }
 
     public static void main(String[] args) {
-        movingCount(2, 3, 1);
+        System.out.println(movingCount(2, 3, 17));
     }
 }
